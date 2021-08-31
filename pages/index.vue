7aa2b4d868943d3b4073b1afacd6b197981918ce
Emrin <emrin95@gmail.com>
2021-08-31 15:10:36 +0200

    wasd

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <Tutorial/>
+  <!-- Please remove this file from your project -->
+  <template>
+    <div>
+      <h1>Welcome</h1>
+      <p>HI THERE</p>
+    </div>
+  </template>
+
 </template>
 
 <script>
