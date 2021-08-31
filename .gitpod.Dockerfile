72587f325dbfe61306d8d742493177c2c81fec42
Emrin <emrin95@gmail.com>
2021-08-31 15:28:32 +0200

    test: 123

--- .gitpod.Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM gitpod/workspace-full:latest
-
-USER gitpod
-
-RUN sudo bash -cl "echo -e \"\n127.0.0.1       emrinangelov\n127.0.0.1       fr.emrinangelov\" >> /etc/hosts"
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,28 @@
+
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  ssr: true,
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'test123',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  // // For deploying at Github Pages without a CNAME
+  // router: {
+  //   base: process.env.NODE_ENV === 'production' ? '/emrinangelov.com/' : '/'
+  // },
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  // // DEV MODE with HMR on Docker:
+  // // 1. Delete node_modules (and maybe yarn.lock) because Linux
+  // // docker run -dp 3000:3000 -w /app -v "$(pwd):/app" --env HOST=0.0.0.0
+  // // --env PORT=3000 node:alpine sh -c "yarn install && yarn dev"
+  // // And have this set to fix HMR.
+  // watchers: {
+  //   webpack: {
+  //     aggregateTimeout: 300,
+  //     poll: 1000
+  //   }
+  // },
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  // // PROD MODE on K8S:
+  // // 1. Build a prod image with Dockerfile (and dockerignore)
+  // // 2. Create a YAML file for its deployment and service
+  // // 3. kubectl apply -f frontend.yaml
 }
